5dd32f35b32ae386508f87429a585940e4af97bd
Shaharuk Shaikh <56402576+shaharuk-yb@users.noreply.github.com>
2025-09-24 14:35:02 +0530

    Update __init__.py

--- vectordb_bench/backend/runner/__init__.py
+++ vectordb_bench/backend/runner/__init__.py
@@ -1,10 +1,11 @@
 from .mp_runner import (
     MultiProcessingSearchRunner,
 )
-from .serial_runner import SerialInsertRunner, SerialSearchRunner
+from .serial_runner import SerialInsertRunner, SerialSearchRunner, MultiProcessingInsertRunner
 
 __all__ = [
     "MultiProcessingSearchRunner",
     "SerialInsertRunner",
     "SerialSearchRunner",
+    "MultiProcessingInsertRunner",
 ]
